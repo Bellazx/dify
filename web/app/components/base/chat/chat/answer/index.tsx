5c97313a82abd2589fd755dfe4b8b477b4bfba9f
Bellazx <dingbella@foxmail.com>
2025-06-23 09:11:19 +0800

    修改answerIcon

--- web/app/components/base/chat/chat/answer/index.tsx
+++ web/app/components/base/chat/chat/answer/index.tsx
@@ -110,7 +110,7 @@ const Answer: FC<AnswerProps> = ({
   return (
     <div className='mb-2 flex last:mb-0'>
       <div className='relative h-10 w-10 shrink-0'>
-        {answerIcon || <AnswerIcon />}
+        {answerIcon || <AnswerIcon iconType="image" imageUrl="/logo/sjtu-site.png" />}
         {responding && (
           <div className='absolute left-[-3px] top-[-3px] flex h-4 w-4 items-center rounded-full border-[0.5px] border-divider-subtle bg-background-section-burn pl-[6px] shadow-xs'>
             <LoadingAnim type='avatar' />
